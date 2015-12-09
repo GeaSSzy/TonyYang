6f7dccb6e697af14bfe8134ac1c45607f187f279
GeaSSzy <geass.zy@gmail.com>
2015-12-09 17:47:20 +0800

    Initial Commit

---
+++ TonyYang/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TonyYang
+//
+//  Created by Geass on 15/12/9.
+//  Copyright © 2015年 Geass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ TonyYang/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TonyYang
+//
+//  Created by Geass on 15/12/9.
+//  Copyright © 2015年 Geass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
